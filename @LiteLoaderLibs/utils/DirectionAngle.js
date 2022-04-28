d0c09a99a1ae7967a7bae19cdf29b6bbc7c3e7fe
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-04-29 07:12:20 +0800

    修正注释

--- @LiteLoaderLibs/utils/DirectionAngle.js
+++ @LiteLoaderLibs/utils/DirectionAngle.js
@@ -1,30 +1,30 @@
 export class DirectionAngle {
-    /**
-     * 创建一个DirectionAngle对象
-     * @param entity {Entity} 实体对象
-     */
+	/**
+	 * 创建一个DirectionAngle对象
+	 * @param entity {Entity} 实体对象
+	 */
 	constructor (entity) {
 		this.entity = entity;
 	}
 
-    /**
-     * @returns {Float} 俯仰角（-90° ~ 90°）
-     */
+	/**
+	 * @returns {number} 俯仰角（-90° ~ 90°）
+	 */
 	get pitch() {
 		return this.entity.pitch;
 	}
 
-    /**
-     * @returns {Float} 偏航角（旋转角 -180° ~ 180°）
-     */
+	/**
+	 * @returns {number} 偏航角（旋转角 -180° ~ 180°）
+	 */
 	get yaw() {
 		return this.entity.yaw > 180 ? this.entity.yaw - 360 : this.entity.yaw;
 	}
 
-    /**
-     * 获取当前朝向
-     * @returns {Int} 北东南西 （0-3）
-     */
+	/**
+	 * 获取当前朝向
+	 * @returns {number} 北东南西 （0-3）
+	 */
 	toFacing() {
 		if (this.yaw < -135 && this.yaw >= 135) {
 			return 0;
