eb463683dfb498298a3c43cae5e5aff529a2cd4a
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-04-29 16:56:00 +0800

    Patch/description (#4)
    * 添加注释
    
    * 添加注释
    
    * 添加注释
    
    * 修正注释
    
    * 修正注释
    
    * Update IntPos.js
    
    * Update FloatPos.js
    
    * Update Device.js

--- @LiteLoaderLibs/libs/mc.js
+++ @LiteLoaderLibs/libs/mc.js
@@ -4,15 +4,36 @@ import { Event } from './Event.js';
 import { Server } from 'cn.nukkit.Server';
 const server = Server.getInstance();
 
+/**
+ * 执行一条命令并返回是否成功
+ * @param cmd {string} 命令
+ * @returns {boolean} 是否成功
+ */
 function runcmd(cmd) {
 	return server.dispatchCommand(server.getConsoleSender(), cmd);
 }
+
+/**
+ * 执行一条命令并返回更多信息
+ * @todo 未实现
+ * @param cmd {string} 命令
+ * @returns {{success: boolean, output: string}} 是否成功与输出信息
+ */
 function runcmdEx(cmd) {
-	// TODO: output是命令的返回
 	return {success: true, output: ''};
 }
+
+/**
+ * 注册一条顶层命令
+ * @todo 未实现
+ * @param cmd {string} 命令
+ * @param description {string} 描述文本
+ * @param [permission=0] {number} 执行所需权限0~2
+ * @param [flag=0x80] {number} 默认值
+ * @param [alias] {number} 命令别名
+ * @returns {Command} 指令对象
+ */
 function newCommand(cmd, description, permission = PermType.Any, flag, alias) {
-	// TODO: 注册一个命令
 	return {};
 }
 
@@ -26,6 +47,11 @@ function listen(event,callback){
 	return Event[event].run(callback);
 }
 
+/**
+ * 获取玩家对象
+ * @param info {string} 玩家名/xuid
+ * @returns {Player|null} 玩家对象
+ */
 function getPlayer(info) {
 	var found = null;
 	if (isNaN(info)) {// 玩家名
@@ -56,6 +82,11 @@ function getPlayer(info) {
 	}
 	return new Player(found);
 }
+
+/**
+ * 获取在线玩家列表
+ * @returns {Player[]} 玩家对象数组
+ */
 function getOnlinePlayers() {
 	var PlayerList = [];
 	for (var player of server.getOnlinePlayers().values()) {
@@ -64,8 +95,13 @@ function getOnlinePlayers() {
 	return PlayerList;
 }
 
+/**
+ * 发给所有玩家一条消息
+ * @param msg {string} 消息内容
+ * @param [type=0] {number} 消息类型
+ * @returns {boolean} 是否成功
+ */
 function broadcast(msg, type = 0) {
-	//TODO: 发给所有玩家
 	for (var player of server.getOnlinePlayers().values()) {
 		sendText(server.getConsoleSender(), player, msg, type);
 	}
--- @LiteLoaderLibs/utils/DirectionAngle.js
+++ @LiteLoaderLibs/utils/DirectionAngle.js
@@ -1,14 +1,31 @@
 export class DirectionAngle {
+	/**
+	 * 创建一个DirectionAngle对象
+	 * @param entity {Entity} 实体对象
+	 */
 	constructor (entity) {
 		this.entity = entity;
 	}
-	get pitch() {// 俯仰角（-90° ~ 90°）    Float
+
+	/**
+	 * @returns {number} 俯仰角（-90° ~ 90°）
+	 */
+	get pitch() {
 		return this.entity.pitch;
 	}
-	get yaw() {// 偏航角（旋转角）	Float
+
+	/**
+	 * @returns {number} 偏航角（旋转角 -180° ~ 180°）
+	 */
+	get yaw() {
 		return this.entity.yaw > 180 ? this.entity.yaw - 360 : this.entity.yaw;
 	}
-	toFacing() {// 北东南西 0123     Int
+
+	/**
+	 * 获取当前朝向
+	 * @returns {number} 北东南西 （0-3）
+	 */
+	toFacing() {
 		if (this.yaw < -135 && this.yaw >= 135) {
 			return 0;
 		} else if (this.yaw < -45 && this.yaw >= -135) {
