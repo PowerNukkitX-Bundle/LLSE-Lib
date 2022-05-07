41223dd5388a20fa32b52c5c4c28d7305ac94f7b
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-05-07 16:56:36 +0800

    Update WebSocket.js

--- @LiteLoaderLibs/libs/WebSocket.js
+++ @LiteLoaderLibs/libs/WebSocket.js
@@ -1,6 +1,6 @@
 import { URI } from 'java.net.URI';
 import { HttpClient } from 'java.net.http.HttpClient';
-//import { ByteBuffer } from 'java.nio.ByteBuffer';
+import { ByteBuffer } from "java.nio.ByteBuffer";
 //import { WebSocket as JWebSocket } from 'java.net.http.WebSocket';
 class WebSocket {
 	constructor() {
@@ -10,30 +10,46 @@ export class WSClient extends WebSocket {
 	constructor() {
 		super();
 		this._events = {};
+		this._lasterror
 	}
 	/**
 	 * 建立链接（同步）
+	 * @todo 由于异步传染性无法正常返回布尔值（多线程也许能够解决这个问题）
 	 * @param wsurl {String} ws链接 ws://ip:port
 	 * @returns {boolean} 是否成功
 	 */
 	connect(wsurl) {
-		this._ws = HttpClient.newHttpClient().newWebSocketBuilder()
-		  .buildAsync(URI.create(wsurl), this);
+		this.connectAsync(wsurl, (succ)=>{return succ});
 		return true;
 	}
 	/**
 	 * 建立链接（异步）
 	 * @param wsurl {String} ws链接 ws://ip:port
-	 * @param callback {Function} args1是否成功
+	 * @param callback {Function} args1是否成功 args2错误信息
 	 */
-	async connectAsync(wsurl, callback) {
-		this._ws = HttpClient.newHttpClient().newWebSocketBuilder()
-		  .buildAsync(URI.create(wsurl), this);
-		/*await new Promise((resolve, reject) => {
-			setTimeout(() => {resolve('time')}, 1000);
-		});*/
-		return callback(true);
+	connectAsync(wsurl, callback = (succ, err)=>{}) {
+		new Promise((resolve, reject) => { 
+			HttpClient.newHttpClient().newWebSocketBuilder()
+			.buildAsync(URI.create(wsurl), this)
+			.whenComplete((webSocket, throable) => {
+				if (Boolean(throable)) {
+					reject(throable);
+				} else {
+					resolve(webSocket);
+				}
+			});
+		}).then(res => {
+			this._ws = res;
+			callback(true);
+		}).catch(e => {
+			this._ws = null;
+			this._lasterror = e;
+			callback(false, e);
+		});
 	}
+	/**
+	 * 是否打开链接
+	 */
 	onOpen(webSocket) {
 		if (this._events['onOpen']) {
 			this._events['onOpen'].apply(this, arguments);
@@ -52,12 +68,14 @@ export class WSClient extends WebSocket {
 		}
 	}
 	onText(webSocket, data, isLast) {
+		print(1)
 		print(data);
 		if (this._events['onTextReceived']) {
 			this._events['onTextReceived'].apply(this, arguments);
 		}
 	}
 	onBinary(webSocket, data, isLast) {
+		print(2)
 		print(data);
 		if (this._events['onBinaryReceived']) {
 			this._events['onBinaryReceived'].apply(this, arguments);
@@ -109,13 +127,17 @@ export class WSClient extends WebSocket {
 		return this._ws.NORMAL_CLOSURE;
 	}
 	/**
-	 * 错误代码
-	 * @param msg {CharSequence} 数据内容
+	 * 发送数据
+	 * @todo 似乎ws有最大长度限制，大数据包需要分片。
+	 * @param msg {ByteBuffer|CharSequence} 数据内容
 	 * @returns {boolean} 是否成功
 	 */
 	send(msg) {
-		print(msg)
-		this._ws.sendText(msg, true);
+		if (msg instanceof ByteBuffer) {
+			this._ws.sendBinary(msg, true);
+		} else {
+			this._ws.sendText(msg, true);
+		}
 		return true;
 	}
 }
