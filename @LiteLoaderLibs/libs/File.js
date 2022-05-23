f36443dbc0fc713101c4715697079572a04b9c39
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-05-23 15:36:02 +0800

    fix 构造函数拼写错误

--- @LiteLoaderLibs/libs/File.js
+++ @LiteLoaderLibs/libs/File.js
@@ -1,7 +1,22 @@
 import * as IO from "../utils/IO.js";
 import { Job } from ":concurrent";
+import { Paths } from "java.nio.file.Paths";
 
 export class File {
+	/**
+	 * @param path {string} 文件路径
+	 * @param mode {string} 文件打开模式
+	 * @param isBinary {boolean} 是否为二进制文件
+	 */
+	constructor (path, mode, isBinary = false) {
+		this._path = Paths.get(path);
+		this._mode = mode;
+		this._isBinary = isBinary;
+		if (!IO.createRAF(this._path, mode)) {
+			throw IO.getPreviousErr(this._path);
+		}
+	}
+
 	static ReadMode = "r";
 	static WriteMode = "rw";
 	static AppendMode = "rws";
@@ -26,25 +41,13 @@ export class File {
 	}
 
 	/**
-	 * @param path {string} 文件路径
-	 * @param mode {string} 文件打开模式
-	 * @param isBinary {boolean} 是否为二进制文件
-	 */
-	constroctor(path, mode, isBinary = false) {
-		this._path = path;
-		this._mode = mode;
-		this._isBinary = isBinary;
-		if (!IO.createRAF(path, mode)) {
-			throw IO.getPreviousErr(path);
-		}
-	}
-	/**
 	 * @param path {string} 文件的路径
 	 */
 	static readFrom(path) {
-		if (IO.createRAF(path)) {
-			const content = IO.readAllText(path);
-			IO.close(path);
+		const _path = Paths.get(path);
+		if (IO.createRAF(_path)) {
+			const content = IO.readAllText(_path);
+			IO.close(_path);
 			return content;
 		}
 		return null;
@@ -54,22 +57,25 @@ export class File {
 	 * @param text {string}
 	 */
 	static writeTo(path, text) {
-		if (IO.createRAF(path)) {
-			if (IO.writeText(path, text)) {
-				IO.close(path);
+		const _path = Paths.get(path);
+		if (IO.createRAF(_path)) {
+			if (IO.writeText(_path, text)) {
+				IO.close(_path);
 				return true;
 			}
 		}
 		return false;
 	}
 	/**
+	 * 向指定文件追加一行
 	 * @param path {string}
 	 * @param text {string}
 	 */
 	static writeLine(path, text) {
-		if (IO.createRAF(path)) {
-			if (IO.seekTo(path, IO.getSize(path) - 1, false) && IO.writeText(path, text + "\n")) {
-				IO.close(path);
+		const _path = Paths.get(path);
+		if (IO.createRAF(_path)) {
+			if (IO.seekTo(_path, IO.getSize(_path) - 1, false) && IO.writeText(_path, text + "\n")) {
+				IO.close(_path);
 				return true;
 			}
 		}
@@ -105,8 +111,8 @@ export class File {
 		return IO.writeLine(this._path, str);
 	}
 	/**
-	 * @param cnt {number}
-	 * @param callback {(result: string|SharedArrayBuffer) => void}
+	 * @param cnt {number} 要读取的字符数 / 字节数
+	 * @param callback {(result: string|SharedArrayBuffer) => void} 获取结果的回调函数
 	 * @returns {Promise<string|SharedArrayBuffer>}
 	 */
 	read(cnt, callback) {
