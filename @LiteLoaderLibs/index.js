8b76747cd03bddbe07fa6acd7664aa5de514368a
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-05-24 13:37:26 +0800

    Update index.js

--- @LiteLoaderLibs/index.js
+++ @LiteLoaderLibs/index.js
@@ -6,6 +6,8 @@ export function close() {
 	print("LiteLoader-Libs close");
 }
 
+export { ll } from './libs/ll.js'
 export { mc } from './libs/mc.js'
 export { File } from './libs/File.js'
 export { WSClient } from './libs/WebSocket.js'
+export { JsonConfigFile } from './libs/JsonConfigFile.js'
