9c724beb8a889966c065b38ee923ab21fb5cb3f4
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-06-02 18:35:08 +0800

    添加log与data，这是过时的api

--- @LiteLoaderLibs/index.js
+++ @LiteLoaderLibs/index.js
@@ -1,3 +1,5 @@
+import { JsonConfigFile } from './libs/JsonConfigFile.js';
+
 export function main() {
 	console.log("LiteLoader-Libs start");
 }
@@ -6,10 +8,37 @@ export function close() {
 	print("LiteLoader-Libs close");
 }
 
-export { Format } from './utils/Format.js'
-export { PermType } from './utils/PermType.js'
-export { ll } from './libs/ll.js'
-export { mc } from './libs/mc.js'
-export { File } from './libs/File.js'
-export { WSClient } from './libs/WebSocket.js'
-export { JsonConfigFile } from './libs/JsonConfigFile.js'
+/**
+ * @deprecated since version LLSE-v0.0.7
+ */
+export var data = {
+	/**
+	 * @see JsonConfigFile
+	 * @todo 实现ini配置文件
+	 */
+	openConfig: function (path, type, defaultContext){
+		if (type === 'json') {
+			return new JsonConfigFile(path, defaultContext);
+		} else if (type === 'ini') {
+			return false;
+		} else {
+			return false;
+		}
+	}
+}
+
+/**
+ * @deprecated since version LLSE-v0.0.7
+ */
+export var log = function() {
+	console.log.apply(this, arguments);
+}
+
+export { Format } from './utils/Format.js';
+export { PermType } from './utils/PermType.js';
+export { system } from './utils/system.js';
+export { ll } from './libs/ll.js';
+export { mc } from './libs/mc.js';
+export { File } from './libs/File.js';
+export { WSClient } from './libs/WebSocket.js';
+export { JsonConfigFile };
