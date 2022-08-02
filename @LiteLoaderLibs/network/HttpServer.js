be83eb2d19d3aef39bb55a788da1d57345eb9b82
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-08-02 09:23:29 +0800

    Update HttpServer.js

--- @LiteLoaderLibs/network/HttpServer.js
+++ @LiteLoaderLibs/network/HttpServer.js
@@ -12,6 +12,12 @@ export class HttpServer {
 
 /**
  * 发送一个异步HTTP(s) Get请求
+ * @example
+     httpGet("https://reqres.in/api/users/2", {
+         aaa: "bbb"
+     }, (status, result) => {
+         console.log(status, result, "???");
+     })
  * @param url {string} 待注册的命令
  * @param [header] {object} 请求头（包括 Get 请求Request header）
  * @param callback {Function} 当请求返回时执行的回调函数，用于回传HTTP(s)响应结果。
@@ -35,13 +41,22 @@ function httpGet(url, header, callback) {
 }
 
 /**
- *  httpPost 异步请求 （其实我更想让他同步
- * @param {String} url
- * @param {array} arg 多参  [,header],data ,type,callback
- * @param {object} header
- * @param  {String} data
- * @param {String} type
- * @param {function} callback
+ * 发送一个异步HTTP(s) Post请求
+ * @example
+     httpPost("https://reqres.in/api/users/2", "dasdaw", "text/plain", (status, result) => {
+         console.log(status, result, "???");
+     })
+ * @param url {string} 待注册的命令
+ * @param header {object} 请求头（包括 Post 请求Request header）
+ * @param data {string} 发送的数据
+ * @param type {object} 发送的 Post 数据类型，形如 text/plain application/x-www-form-urlencoded 等
+ * @param callback {Function} 当请求返回时执行的回调函数，用于回传HTTP(s)响应结果。
+ *     注：参数callback的回调函数原型：function(status,result)
+ *     status : Integer
+ *     返回的HTTP(s)响应码，如200代表请求成功
+ *     result : String
+ *     返回的具体数据
+ * @returns {boolean} 是否成功发送请求
  */
 function httpPost(url, ...arg) {
     // header, data, type, callback
@@ -61,11 +76,3 @@ function httpPost(url, ...arg) {
 }
 
 export let network = {httpGet, httpPost}
-// httpGet("https://reqres.in/api/users/2", {
-//     aaa: "bbb"
-// }, (status, result) => {
-//     console.log(status, result, "???");
-// })
-// httpPost("https://reqres.in/api/users/2", "dasdaw", "text/plain", (status, result) => {
-//     console.log(status, result, "???");
-// })
