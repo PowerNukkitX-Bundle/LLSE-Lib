86b9e5ae8b71f47f65215a5c327abb6bacbb6fc8
CoolLoong <1542536763@qq.com>
2022-08-05 21:18:42 +0800

    Revert "测试失败情况"
    This reverts commit a1f52d7ba565dd8a394f51dec3688d4faa4d7f0d.

--- @test/object/TestEntity.js
+++ @test/object/TestEntity.js
@@ -17,7 +17,7 @@ export const TestEntity = () => {
     //注册测试套件
     JSAssert.addTestSuite("Test Entity", {
         testProperty: function () {
-            assertThat(entity.name).equals(" Pig", "name属性读取异常");
+            assertThat(entity.name).equals("Pig", "name属性读取异常");
             assertThat(entity.type).equals("Pig", "type属性读取异常");
             assertThat(isNumber(entity.id)).isTrue("id属性读取异常");
             assertThat(entity.pos.toString()).equals(`{"x":0,"y":300,"z":0,"dim":"world","dimid":0}`, "pos属性读取异常");
