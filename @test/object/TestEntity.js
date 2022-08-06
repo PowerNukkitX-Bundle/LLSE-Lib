bcf1d27d0eb44d89f77c4098e4bab1e10ff03074
CoolLoong <1542536763@qq.com>
2022-08-06 10:20:41 +0800

    修复package.yml (#33)
    * 修复package.yml
    
    * 修复package.yml2
    
    * 添加测试，修复一些bug

--- @test/object/TestEntity.js
+++ @test/object/TestEntity.js
@@ -34,6 +34,26 @@ export const TestEntity = () => {
             } catch (e) {
                 assertThat(false).isTrue("uniqueId属性异常");
             }
+        },
+        testFunction: function () {
+            entity.teleport(0, 280, 0, 0);
+            assertThat(entity.pos.toString()).equals(`{"x":0,"y":280,"z":0,"dim":"world","dimid":0}`, "teleport函数执行异常");
+            entity.hurt(2);
+            assertThat(entity.health).equals(18, "hurt函数执行异常");
+            assertThat(!entity.isPlayer()).isTrue("isPlayer函数执行异常");
+            assertThat(!entity.isItemEntity()).isTrue("isItemEntity函数执行异常");
+            assertThat(JSON.parse(entity.getBlockStandingOn().toString())["name"]).equals("Air", "getBlockStandingOn函数执行异常");
+            assertThat(entity.addTag("testtag")).isTrue("addTag函数执行异常");
+            assertThat(entity.getAllTags()).equals(["testtag"], "getAllTags函数执行异常");
+            assertThat(entity.hasTag("testtag")).isTrue("hasTag函数执行异常");
+            assertThat(entity.removeTag("testtag")).isTrue("removeTag函数执行异常");
+        },
+        testNBT: function () {
+            let nbt = entity.getNbt();
+            assertThat(nbt.toSNBT()).equals(`{"Motion":[0.0d,0.0d,0.0d],"FallDistance":0.0f,"Pos":[0.0d,300.0d,0.0d],"Health":20.0f,"Fire":0s,"Invulnerable":0b,"Scale":1.0f,"Air":300s,"OnGround":0b,"Rotation":[0.0f,0.0f],"Tags":[]}`, "getNbt异常");
+            let newNBT = nbt.setFloat("Health", 18);
+            entity.setNbt(newNBT);
+            assertThat(entity.getNbt().toSNBT()).equals(`{"Motion":[0.0d,0.0d,0.0d],"FallDistance":0.0f,"Pos":[0.0d,300.0d,0.0d],"Health":18.0f,"Fire":0s,"Invulnerable":0b,"Scale":1.0f,"Air":300s,"OnGround":0b,"Rotation":[0.0f,0.0f],"Tags":[]}`, "setNbt异常");
         }
     });
 };
