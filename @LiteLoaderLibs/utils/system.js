f5413cd29a90c6ed4646d68c7cf0652605236c98
Mcayear <48203962+Mcayear@users.noreply.github.com>
2022-08-13 22:28:54 +0800

    Update/money api (#40)
    * 添加经济插件软依赖项
    
    * Update data.js
    
    * Update Player.js
    
    * 增加数据库
    
    * 修复异步导致无法加载sqlite库的问题，更新链接为原子资源链接
    
    * 更灵活的 getTimeStr、getTimeObj

--- @LiteLoaderLibs/utils/system.js
+++ @LiteLoaderLibs/utils/system.js
@@ -2,32 +2,36 @@ import { UUID } from 'java.util.UUID';
 
 /**
  * 系统API对象
+ * 
+ * 不可实例化
  */
 export class system {
 
     /**
      * 获取当前时间字符串，形如2021-04-03 19:15:01
+     * @param [date] {Date} 时间对象（可选）
      * @returns {string} 返回格式时间字符串
      */
-    static getTimeStr() {
-        const t = system.getTimeObj();
+    static getTimeStr(date) {
+        const t = system.getTimeObj(date);
         return t.Y + '-' + bu0(t.M) + '-' + bu0(t.D) + ' ' + bu0(t.h) + ':' + bu0(t.m) + ':' + bu0(t.s);
     }
 
     /**
      * 获取当前的时间对象
+     * @param [date] {Date} 时间对象（可选）
      * @returns {object} 返回一个对象
      */
-    static getTimeObj() {
-        const myDate = new Date();
+    static getTimeObj(date = new Date()) {
+        if (typeof(date) != 'object') date = new Date(date);// 转为Date
         return {
-            Y: myDate.getFullYear(),// 年份数值（4 位）
-            M: myDate.getMonth() + 1,// 月份数值1-12
-            D: myDate.getDate(),// 天数数值1-31
-            h: myDate.getHours(),// 小时数值0-23（24 小时制）
-            m: myDate.getMinutes(),// 分钟数值0-59
-            s: myDate.getSeconds(),// 秒数值0-59
-            ms: myDate.getMilliseconds()// 毫秒数值0-999
+            Y: date.getFullYear(),// 年份数值（4 位）
+            M: date.getMonth() + 1,// 月份数值1-12
+            D: date.getDate(),// 天数数值1-31
+            h: date.getHours(),// 小时数值0-23（24 小时制）
+            m: date.getMinutes(),// 分钟数值0-59
+            s: date.getSeconds(),// 秒数值0-59
+            ms: date.getMilliseconds()// 毫秒数值0-999
         }
     }
 
